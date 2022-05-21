cb3297fc2544f2c4ef539d1f3a68333e629225c6
alex <tgfc.alex@gmail.com>
2022-05-21 23:40:00 +0800

    self_link

--- cloud-nat.tf
+++ cloud-nat.tf
@@ -16,9 +16,9 @@ resource "google_compute_router" "router" {
 }
 
 
-resource "google_compute_address" "terraform-nat-ip" {
+resource "google_compute_address" "terraform_nat_ip" {
   name = "terraform-nat-ip"
-  region       = "${var.region}"
+  region = "${var.region}"
 }
 
 #Create Cloud nat
@@ -30,6 +30,6 @@ module "cloud-nat" {
   router                             = google_compute_router.router.name
   name                               = "nat-gke"
   nat_ip_allocate_option             = "MANUAL_ONLY"
-  nat_ips                            = "google_compute_address.terraform-nat-ip.address
+  nat_ips                            = "google_compute_address.terraform_nat_ip.self_link
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
